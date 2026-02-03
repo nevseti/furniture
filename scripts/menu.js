4e99d2113668420dd8171dd670426d6dd51311eb
Edem Shukuriev <2nevseti6@mail.ru>
2026-02-03 13:58:24 +0300

    fix: js null check for scroll button

--- scripts/menu.js
+++ scripts/menu.js
@@ -48,19 +48,21 @@ window.addEventListener('resize', () => {
 // Кнопка "Наверх"
 const scrollTopButton = document.getElementById('scrollTop');
 
-// Показываем кнопку после скролла
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-        scrollTopButton.classList.add('visible');
-    } else {
-        scrollTopButton.classList.remove('visible');
-    }
-});
-
-// Прокрутка к началу страницы
-scrollTopButton.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+if (scrollTopButton) {
+    // Показываем кнопку после скролла
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            scrollTopButton.classList.add('visible');
+        } else {
+            scrollTopButton.classList.remove('visible');
+        }
     });
-});
+
+    // Прокрутка к началу страницы
+    scrollTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
